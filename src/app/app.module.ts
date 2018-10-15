992417e803b28edb6c9fedc271faf353d872334a
Gers <gerson@EddyEddy.local>
2018-10-15 10:47:33 -0500

    primera fase de creacion y diseño

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+//rutas
+import {AppRoutingModule} from './rutas';
+
+//component
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BodyComponent } from './components/home/body/body.component';
+import { RegistrosComponent } from './components/forms/registros/registros.component';
+import { ContactoComponent } from './components/forms/contacto/contacto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    BodyComponent,
+    RegistrosComponent,
+    ContactoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/rutas.ts
@@ -0,0 +1,24 @@
+import { RouterModule,Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BodyComponent } from './components/home/body/body.component';
+import { RegistrosComponent } from './components/forms/registros/registros.component';
+import { ContactoComponent } from './components/forms/contacto/contacto.component';
+
+const APP_ROUTES:Routes =[
+
+{path:'home',component:HomeComponent},
+{path:'body',component:BodyComponent},
+{path:'**',pathMatch: 'full', redirectTo:'home'}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(APP_ROUTES)],
+
+exports: [RouterModule]
+})
+export class AppRoutingModule { }
